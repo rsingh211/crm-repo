73dab5cbaddc8b7e5a6da056ba2894b7c04252e2
rsingh211 <rsingh15@academic.rrc.ca>
2025-03-12 17:56:01 -0500

    Added routes for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  # ActiveAdmin setup
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Customer Routes
+  root "customers#index"
+  get "customers/alphabetized", to: "customers#alphabetized"
+  get "customers/missing_email", to: "customers#missing_email"
+  resources :customers
+
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # PWA (optional, if needed in future)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
+
