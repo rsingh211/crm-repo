f13fcfc8a689a5de8b9439ff1779ba714413220f
rsingh211 <rsingh15@academic.rrc.ca>
2025-03-12 17:49:00 -0500

    Configured ActiveStorage for Customer images

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -11,5 +11,37 @@ ActiveAdmin.register Customer do
       end
       f.actions
     end
+  
+    index do
+      selectable_column
+      id_column
+      column :full_name
+      column :phone_number
+      column :email
+      column "Image" do |customer|
+        if customer.image.attached?
+          image_tag customer.image.variant(resize: "50x50"), class: "admin-customer-img"
+        else
+          "No Image"
+        end
+      end
+      actions
+    end
+  
+    show do
+      attributes_table do
+        row :full_name
+        row :phone_number
+        row :email
+        row :notes
+        row "Image" do |customer|
+          if customer.image.attached?
+            image_tag customer.image, size: "100x100"
+          else
+            "No Image"
+          end
+        end
+      end
+    end
   end
   
